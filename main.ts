0a88fdde7754657cea15382dfbb071d9ef0f9b50
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 18:18:53 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+function adivinhar_numero () {
+	
+}
+/**
+ * Na pág. 216 item 6, nossa sugestão é utilizar o bloco NO LOGOTIPO PRESSIONADO. Caso você esteja utilizando a micro:bit v1, esta funcionalidade não estará disponível, então você pode substituir NO LOGOTIPO PRESSIONADO pelo bloco EM AGITAR.
+ */
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
